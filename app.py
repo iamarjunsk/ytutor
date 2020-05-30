516c95da12b56ef1a9bf90162dec19d4eb997e4d
Arjun SK <dev.arjunsk@gmail.com>
2020-05-30 09:50:45 +0530

    v 0.7

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import smtplib
 app=Flask(__name__)
 
 ENV='dev'
-if ENV == 'dev':
+if ENV == 'prod':
     app.debug=True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/yt'
 else:
