fe5e70b8fc780e3b7662d5067f82e77164e01785
Arjun SK <dev.arjunsk@gmail.com>
2020-08-25 20:34:49 +0530

    initialize max length in tutreg

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from secrets import secretkey,email,emailpassword,db
 
 app=Flask(__name__)
 
-ENV='dev'
+ENV='prod'
 if ENV == 'dev':
     app.debug=True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/yt'
